448fc3d41fe9eb6e80443498712d9c480f8ae172
MSoup <19704779+MSoup@users.noreply.github.com>
2021-02-20 11:19:19 +0900

    added reset and score keeper

--- main.js
+++ main.js
@@ -15,14 +15,17 @@ const gameboard = (() => {
   // display Start Game in Middle
   board.querySelector(".cell-4").textContent = "Start Game";
 
+  const displayMessage = (message) => {
+    messageDisplay.textContent = message;
+  };
+
   const writeToCell = (cell, mark) => {
     if (gameboard[cell][1]) {
-      messageDisplay.textContent = "Cannot place there!";
+      displayMessage("Cannot place there!");
       throw "Non empty cell!";
     }
 
     gameboard[cell][1] = mark;
-    isGameOver();
     renderPage();
   };
 
@@ -50,33 +53,57 @@ const gameboard = (() => {
     ];
     for (i = 0; i < winningCombinations.length; i++) {
       if (winningCombinations[i].every((num) => gameboard[num][1] === "X")) {
-        messageDisplay.textContent = "Winner is X";
-        break;
+        displayMessage("Winner is X");
+        P1.incScore();
+        return true;
       } else if (
         winningCombinations[i].every((num) => gameboard[num][1] === "O")
       ) {
-        messageDisplay.textContent = "Winner is O";
-        break;
+        displayMessage("Winner is O");
+        P2.incScore();
+        return true;
       }
     }
 
     if (!gameboard.some((group) => group[1] === null)) {
       console.log("Draw");
+      return true;
     }
+    return false;
   };
 
   const resetGame = () => {
     gameboard.forEach((cell) => (cell[1] = null));
+    displayMessage("Game Reset");
+
     renderPage();
   };
 
-  return { renderPage, writeToCell, isGameOver, resetGame };
+  const placeRandMove = (mark) => {
+    let availableCells = gameboard.filter((cell) => cell[1] === null);
+    if (!availableCells) {
+      return displayMessage("No more available moves");
+    }
+    let choice = Math.floor(Math.random() * Math.floor(availableCells.length));
+    writeToCell(availableCells[choice][0], mark);
+  };
+  return {
+    renderPage,
+    writeToCell,
+    isGameOver,
+    resetGame,
+    displayMessage,
+    placeRandMove,
+  };
 })();
 
 const Player = (name, mark) => {
+  let score = 0;
   const getName = () => console.log("Hi I'm " + name);
   const makeMove = (cell) => gameboard.writeToCell(cell, mark);
-  return { getName, makeMove };
+  const incScore = () => score++;
+  const getScore = () => score;
+  return { getName, makeMove, incScore, getScore };
 };
 
 const P1 = Player("Mark", "O");
@@ -84,21 +111,47 @@ const P2 = Player("Jim", "X");
 
 const controller = (() => {
   let activePlayer = "X";
-  const middleCell = document.querySelector(".cell-4");
   const cells = document.querySelector(".game-board");
+  // buttons
+  const resetButton = document.querySelector(".reset");
+  const undoButton = document.querySelector(".undo");
+  const randMoveButton = document.querySelector(".randMove");
+
+  handleReset = function () {
+    gameboard.resetGame();
+    cells.addEventListener("click", handlePlaceMark);
+    randMoveButton.addEventListener("click", handleRandomMove);
+  };
 
   handlePlaceMark = function (event) {
     const cellClass = event.target.className;
     const cellNumber = cellClass[cellClass.length - 1];
     gameboard.writeToCell(cellNumber, activePlayer);
     activePlayer = activePlayer === "X" ? "O" : "X";
+    gameboard.displayMessage("Current Player: " + activePlayer);
+    if (gameboard.isGameOver()) {
+      document.querySelector(".score1").textContent = P1.getScore();
+      document.querySelector(".score2").textContent = P2.getScore();
+
+      cells.removeEventListener("click", handlePlaceMark);
+      randMoveButton.removeEventListener("click", handleRandomMove);
+    }
+  };
+
+  handleRandomMove = function () {
+    gameboard.placeRandMove(activePlayer);
+    activePlayer = activePlayer === "X" ? "O" : "X";
+    if (gameboard.isGameOver()) {
+      console.log("Game over");
+      document.querySelector(".score1").textContent = P1.getScore();
+      document.querySelector(".score2").textContent = P2.getScore();
+
+      cells.removeEventListener("click", handlePlaceMark);
+      randMoveButton.removeEventListener("click", handleRandomMove);
+    }
   };
 
   cells.addEventListener("click", handlePlaceMark);
+  resetButton.addEventListener("click", handleReset);
+  randMoveButton.addEventListener("click", handleRandomMove);
 })();
-
-// P1.makeMove(0);
-// P2.makeMove(4);
-// P1.makeMove(1);
-// P2.makeMove(5);
-// P1.makeMove(2);
